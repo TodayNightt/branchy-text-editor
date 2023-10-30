180731cd82c38a50f4986e33551f2d5272ef9253
TodayNightt <sd.teoh.tsd@gmail.com>
2023-10-30 11:29:40 +0900

    Revert "Revert "Implement the frontend for the file explorer component""
    This reverts commit a6c4459930283d59b4c7443c06b6952d3be76ec4.

--- src/stateStore.ts
+++ src/stateStore.ts
@@ -1,19 +1,39 @@
 import { createStore } from "solid-js/store";
+import { FileSystemInfo, OpenFile, getFileSystemInfo,openFile } from "./bindings";
 
-type File = {
-    id:number,
-    name :string,
-    source_code: string,
+type Store = {
+  fileSystem: FileSystemInfo,
+  openedFile:Array<OpenFile>
+}
+
+export const invokeChangeDir = async (dir: string | null) => {
+  const files = await catchIfAny(getFileSystemInfo(dir));
+  if(files) setStore("fileSystem",files);
+}
+
+export const invokeOpenFile = async (path: string) => {
+  const file = await catchIfAny(openFile(path));
+  if (file) setStore('openedFile', prev => [...prev, file]);
 }
 
 
-
-const [store, setStore] = createStore({
-    opened: Array<File>,
+//https://gist.github.com/karlhorky/3593d8cd9779cf9313f9852c59260642
+export async function catchIfAny<T>(promise: Promise<T>): Promise<T | null>  {
+  try {
+    return await promise;
+  } catch (error) {
+    console.log(error)
+  }
+  return null;
 }
-);
 
+const [store, setStore] = createStore<Store>({
+  fileSystem: {
+    current_directory: "",
+    directory_items : []
+  },
+  openedFile:[]
+});
 
-export { store, setStore };
-
+export { store }
 
