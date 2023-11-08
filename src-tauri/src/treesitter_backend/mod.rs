ad872358c3f23b78325d1d6880f4d3fc9694b570
TodayNightt <sd.teoh.tsd@gmail.com>
2023-11-08 23:39:30 +0900

    Added support for typescript and html
    However there is a bug in the highlighter that it will crash when
    subtract overflow

--- src-tauri/src/treesitter_backend/mod.rs
+++ src-tauri/src/treesitter_backend/mod.rs
@@ -8,17 +8,18 @@ pub mod parser;
 pub fn get_tree_sitter_language(lang: &Lang) -> Language {
     match lang {
         Lang::Javascript => tree_sitter_javascript::language(),
-        Lang::Typescript => tree_sitter_javascript::language(),
+        Lang::Typescript => tree_sitter_typescript::language_typescript(),
         Lang::Rust => tree_sitter_rust::language(),
+        Lang::Html => tree_sitter_html::language(),
         _ => tree_sitter_javascript::language(),
     }
 }
 pub fn get_query_from_each_language(language: &Lang) -> &str {
     match language {
         Lang::Javascript => tree_sitter_javascript::HIGHLIGHT_QUERY,
+        Lang::Typescript => tree_sitter_typescript::HIGHLIGHT_QUERY,
         Lang::Rust => tree_sitter_rust::HIGHLIGHT_QUERY,
+        Lang::Html => tree_sitter_html::HIGHLIGHT_QUERY,
         _ => tree_sitter_javascript::HIGHLIGHT_QUERY,
     }
 }
-
-
