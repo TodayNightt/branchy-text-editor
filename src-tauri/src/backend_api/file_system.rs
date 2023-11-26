94b58aa75abf9b1c0f92232f4530909bd4fe22aa
TodayNightt <sd.teoh.tsd@gmail.com>
2023-11-26 14:26:37 +0900

    Make recursion 4 times for fetching directory items

--- src-tauri/src/backend_api/file_system.rs
+++ src-tauri/src/backend_api/file_system.rs
@@ -19,7 +19,7 @@ pub fn get_file_system_info(dir: Option<String>) -> Result<FileSystemInfo, Error
         if !path_buf.is_absolute() {
             path_buf = path_buf.absolutize().unwrap().to_path_buf();
         }
-        let directory_items = get_directory_items(&path_buf, 2)?;
+        let directory_items = get_directory_items(&path_buf, 4)?;
         return Ok(FileSystemInfo::create(
             path_buf
                 .into_os_string()
@@ -29,7 +29,7 @@ pub fn get_file_system_info(dir: Option<String>) -> Result<FileSystemInfo, Error
         ));
     }
     let home_dir = home_dir().unwrap().absolutize().unwrap().to_path_buf();
-    let directory_items = get_directory_items(&home_dir, 2)?;
+    let directory_items = get_directory_items(&home_dir, 4)?;
     Ok(FileSystemInfo::create(
         home_dir
             .into_os_string()
