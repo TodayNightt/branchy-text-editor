d7beb042065c0ab5ad7d6241c1c09ee8328d4684
TodayNightt <sd.teoh.tsd@gmail.com>
2025-07-27 17:14:19 +0800

    bump to tauriV2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,14 +5,14 @@ use branchy_text_editor::{
     backend_api::{editor_info::*, file_data::*, file_system::*},
     StateManager,
 };
-use specta::collect_types;
+use tauri_specta::{collect_commands, Builder};
 use tauri::Manager;
-use tauri_specta::ts;
+use specta_typescript::Typescript;
 
 fn main() {
-    #[cfg(debug_assertions)]
-    ts::export(
-        collect_types![
+    let mut builder = Builder::<tauri::Wry>::new()
+        // Then register them (separated by a comma)
+        .commands(collect_commands![
             get_file_system_info,
             open_file,
             get_source_code_if_any,
@@ -24,28 +24,20 @@ fn main() {
             get_editor_config,
             get_tokens_legend,
             set_highlights,
-            get_currently_supported_language
-        ],
-        "../src/backendApi/bindings.ts",
-    )
-    .unwrap();
+            get_currently_supported_language]
+        );
+
+    #[cfg(debug_assertions)] // <- Only export on non-release builds
+    builder
+        .export(Typescript::default(), "../src/bindings.ts")
+        .expect("Failed to export typescript bindings");
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            open_file,
-            get_file_system_info,
-            get_source_code_if_any,
-            close_file,
-            save_file,
-            handle_file_changes,
-            reset,
-            get_current_language_theme,
-            get_editor_config,
-            get_tokens_legend,
-            set_highlights,
-            get_currently_supported_language
-        ])
+        // and finally tell Tauri how to invoke them
+        .invoke_handler(builder.invoke_handler())
         .setup(|app| {
+            // This is also required if you want to use events
+            builder.mount_events(app);
             let app_handle = app.handle();
             let config_dir = app_handle.path_resolver().app_local_data_dir();
             let state_manager = StateManager::new(config_dir)?;
